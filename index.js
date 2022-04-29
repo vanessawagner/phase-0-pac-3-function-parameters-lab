b83ce1c690151be6876f9231a1e52ca4efe8cad0
Vanessa Wagner <vmwagner@gmail.com>
2022-04-29 06:36:45 -0500

    commit tree c7393ce2770e9c468576c622eb8768d22bb9d3a0

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction(name) {
+    const name = 'vanessa'
+    console.log(`Hi, my name is ${name}.`);
+}
+
+
+function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
